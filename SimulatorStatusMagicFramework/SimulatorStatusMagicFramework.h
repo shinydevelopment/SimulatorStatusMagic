4a746e32eda06ee6cfa193b94d2591d37ffafa89
Shri <Shikantreddy.Tekale@ordnancesurvey.co.uk>
2015-09-16 17:04:25 +0100

    Added aggregator

--- SimulatorStatusMagicFramework/SimulatorStatusMagicFramework.h
+++ SimulatorStatusMagicFramework/SimulatorStatusMagicFramework.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char SimulatorStatusMagicFrameworkVersionString
 
 // In this header, you should import all the public headers of your framework using statements like #import <SimulatorStatusMagicFramework/PublicHeader.h>
 
-
+#import <SimulatorStatusMagicFramework/SDStatusBarManager.h>
