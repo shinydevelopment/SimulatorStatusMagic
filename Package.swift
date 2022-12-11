adf0e6a052c7239a582de6df97ba175b3aefaecf
Daniel Springer <19246409+DaniSpringer@users.noreply.github.com>
2022-12-11 00:33:45 -0500

    fixes

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .library(
             name: "SimulatorStatusMagic",
             type: .dynamic,
-            targets: ["SimulatorStatusMagic"]),
+            targets: ["SimulatorStatusMagic"])
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
